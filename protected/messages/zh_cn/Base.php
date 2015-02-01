c39360d63ae9944eb3b34359af0943eccf33aaed
Your Name <you@example.com>
2015-02-02 01:02:23 +0800

    ok

--- protected/messages/zh_cn/Base.php
+++ protected/messages/zh_cn/Base.php
@@ -7,45 +7,45 @@
  */
 return array(
 
-    'FIND HOME FOR SALE.'=>'查找在售房源',
+    'FIND HOME FOR SALE.' => '查找在售房源',
 
-    'Enter Search Keywords'=>'输入搜索关键字',
+    'Enter Search Keywords' => '输入搜索关键字',
 
-    'area'=>'城市',
+    'area' => '城市',
 
-    'subarea'=>'小区',
+    'subarea' => '小区',
 
-    'UPDATE FILTERS'=>'搜索',
+    'UPDATE FILTERS' => '搜索',
 
-    'VIEW DETAILS'=>'查看详情',
+    'VIEW DETAILS' => '查看详情',
 
-    'Contact'=>'联系我们',
+    'Contact' => '联系我们',
 
-    'Login'=>'登录',
+    'Login' => '登录',
 
-    'Logout'=>'注销',
+    'Logout' => '注销',
 
-    'News'=>'新闻中心',
+    'News' => '新闻中心',
 
-    'HOUSING TYPES'=>'房屋类型',
+    'HOUSING TYPES' => '房屋类型',
 
-    'AGE DISTRIBUTION'=>'房年分布',
+    'AGE DISTRIBUTION' => '房年分布',
 
-    'FEATURED PROPERTIES'=>'推荐房源',
+    'FEATURED PROPERTIES' => '推荐房源',
 
-    'LAST VIEWED'=>'浏览过的房源',
+    'LAST VIEWED' => '浏览过的房源',
 
-    'Home Size in Sq. Ft.'=>'房屋面积分布',
+    'Home Size in Sq. Ft.' => '房屋面积分布',
 
-    'Year Built'=>'建造年份',
+    'Year Built' => '建造年份',
 
     'SEARCH' => '搜索',
 
     'MAP SEARCH' => '地图搜索',
 
-    'HOUSE FOR SALE' =>'在售房源',
+    'HOUSE FOR SALE' => '在售房源',
 
-    'CONDO' =>'公寓',
+    'CONDO' => '公寓',
 
     'Any price range' => '价格区间',
 
@@ -55,14 +55,46 @@ return array(
 
     'OK' => '确认',
 
-    'House Types' =>'房型',
+    'House Types' => '房型',
 
-    'Recommended for you' =>'为您推荐',
+    'Recommended for you' => '为您推荐',
 
     'LIST SEARCH' => '搜索列表',
 
-    'HISTORY'=> '历史',
+    'HISTORY' => '历史',
 
-    'PROPERTY HISTORY' =>'价格历史',
+    'PROPERTY HISTORY' => '价格历史',
+
+    'More' => '更多选项',
+
+    'Search BedRoom' => '睡&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;房',
+
+    'Toilets' => '洗&nbsp;&nbsp;手&nbsp;&nbsp;间',
+
+    'LivingArea' => '居住面积',
+
+    'LandArea' => '土地面积',
+
+    'Age' => '房&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;龄',
+
+
+    'Unit Number' => '单位号码',
+
+    'Street' => '街&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;道',
+
+    'Streets Species' => '街道种类',
+
+    'Number' => '门牌号码',
+
+    'Zip Code' => '邮&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;编',
+
+    'MLS Code' => 'MLS编号',
+
+    'Sales Time' => '上市时间',
+
+    'Plan' => '规&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;划',
+
+    'PID Code' => 'PID识别码',
+
 
 );
